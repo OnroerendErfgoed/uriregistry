dfbcbd87a1029d0c5621a4a782029aa2621b0aaf
Koen Van Daele <koen_van_daele@telenet.be>
2015-05-19 16:23:26 +0200

    Add test to parse settings.

--- tests/test_general.py
+++ tests/test_general.py
@@ -2,9 +2,20 @@ import pytest
 
 import os
 
-from uriregistry import _load_configuration
+from uriregistry import (
+    _parse_settings,
+    _load_configuration
+)
 
 class TestGeneral:
+
+    def test_parse_settings(self):
+        settings = {
+            'uriregistry.config': os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test.cfg')
+        }
+        args = _parse_settings(settings)
+        assert 'config' in args
+        assert args['config'] == os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test.cfg')
 
     def test_load_configuration(self):
         cfg = _load_configuration(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test.cfg'))
