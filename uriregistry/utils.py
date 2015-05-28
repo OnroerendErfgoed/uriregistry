b1e6e08ef7cd7b66762f1b74a2551a7994ecdc2d
Koen Van Daele <koen_van_daele@telenet.be>
2015-05-28 11:49:18 +0200

    Fix missing requests declaration

--- uriregistry/utils.py
+++ uriregistry/utils.py
@@ -3,6 +3,13 @@
 import logging
 log = logging.getLogger(__name__)
 
+import requests
+
+try:
+    from urllib import urlencode
+except ImportError:
+    from urllib.parse import urlencode
+
 from pyramid_urireferencer.models import ApplicationResponse
 
 def query_application(app, uri):
@@ -14,10 +21,10 @@ def query_application(app, uri):
     :rtype pyramid_urireferencer.models.ApplicationResponse:
     """
     try:
-        url = '{0}?{1}'.format(app.service_url, urllib.urlencode({'uri': uri}))
+        url = '{0}?{1}'.format(app.service_url, urlencode({'uri': uri}))
         r = requests.get(url)
         a = ApplicationResponse.load_from_json(r.json())
         return a
-    except:
+    except Exception as e:
         log.error('Could not check if uri %s is known to app %s (uri: %s)' % (uri, app.title, app.uri))
         return ApplicationResponse(app.title, app.uri, app.service_url, False, None, None, None)
