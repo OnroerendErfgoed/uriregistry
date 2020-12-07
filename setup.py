1bdf6adf8fc5eb96eb7f4f361c69a327aae3cb6b
Wouter Claeys <wouter.claeys@vlaanderen.be>
2020-12-07 11:39:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ requires = [
     ]
 
 setup(name='uriregistry',
-      version='0.1.2',
+      version='0.2.0',
       description='A central URI registry that tracks where a certain URI is being used.',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
