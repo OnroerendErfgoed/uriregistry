7878d5dceb1824c035ac5271abb1a24334af9204
Tim Van Campenhout <45393594+vancamti@users.noreply.github.com>
2026-05-11 15:12:49 +0200

    #108 add test coverage reporting to PRs (#109)

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,7 @@
+import json
+
+import httpretty
+import pytest
 from pyramid_urireferencer.models import ApplicationResponse
 
 from uriregistry.models import Application
@@ -17,3 +21,33 @@ class TestUtils:
         assert r.service_url == app.service_url
         assert r.title == app.title
         assert r.success is False
+
+    @pytest.fixture
+    def http_mock(self):
+        httpretty.enable(allow_net_connect=False)
+        yield httpretty
+        httpretty.disable()
+        httpretty.reset()
+
+    def test_query_application_success(self, http_mock):
+        app = Application(
+            "http://uri/app", "app_name", "http://url/app/references"
+        )
+        payload = {
+            "query_uri": "http://id.erfgoed.net/foobar/2",
+            "title": app.title,
+            "uri": app.uri,
+            "service_url": app.service_url,
+            "success": True,
+            "has_references": True,
+            "count": 4,
+            "items": [],
+        }
+        http_mock.register_uri(
+            http_mock.GET, app.service_url, body=json.dumps(payload)
+        )
+        r = query_application(app, "http://id.erfgoed.net/foobar/2")
+        assert isinstance(r, ApplicationResponse)
+        assert r.success is True
+        assert r.has_references is True
+        assert r.count == 4
